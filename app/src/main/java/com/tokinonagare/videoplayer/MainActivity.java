f95a963d308bb0b103498671a1aafff6c348061c
tokinonagare <timemahou@gmail.com>
2015-09-03 13:15:00 +0800

    Add MediaController

--- app/src/main/java/com/tokinonagare/videoplayer/MainActivity.java
+++ app/src/main/java/com/tokinonagare/videoplayer/MainActivity.java
@@ -3,6 +3,7 @@ package com.tokinonagare.videoplayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.VideoView;
+import android.widget.MediaController;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +14,11 @@ public class MainActivity extends AppCompatActivity {
 
         final VideoView tokinonagareVideoView = (VideoView) findViewById(R.id.tokinonagareVideoView);
         tokinonagareVideoView.setVideoPath("https://www.thenewboston.com/forum/project_files/006_testVideo.mp4");
+
+        //Player controls(play, pause, stop, etc...)
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(tokinonagareVideoView);
+        tokinonagareVideoView.setMediaController(mediaController);
 
         tokinonagareVideoView.start();
     }
